a2ce260af09606e2f2a613dfa069de52bcd7a1a2
Your Name <you@example.com>
2024-09-10 16:16:58 +0200

    Added the api

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+export const fetchProducts = async (page = 1) => {
+  const response = await fetch(`https://next-ecommerce-api.vercel.app/products?page=${page}&limit=20`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch products');
+  }
+  const data = await response.json();
+  return data;
+};
+
+export const fetchProductById = async (id) => {
+  const response = await fetch(`https://next-ecommerce-api.vercel.app/products/${id}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch product');
+  }
+  const data = await response.json();
+  return data;
+};
